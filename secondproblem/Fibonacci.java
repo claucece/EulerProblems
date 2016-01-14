a847f3a64709587c174c61864f6ea49125116842
Sofia Celi <sceli@thoughtworks.com>
2016-01-14 11:19:41 -0500

    second problem

---
+++ secondproblem/Fibonacci.java
@@ -0,0 +1,23 @@
+class Fibonacci {
+   int fibo = 0;
+   int prev = 0;
+   int current = 1;
+   int grand_total = 0;
+   int sum = 0;
+   boolean run = true;
+   
+   public void getEvens(){
+   while (fibo < 4000000) {
+     fibo = current + prev;
+     prev = current;
+     current = fibo;
+     if (fibo % 2 == 0) {
+       sum += fibo;
+     }
+     System.out.println(current + " " + prev );
+     }
+     System.out.println(sum);
+    }
+  }
+     
+     
---
+++ secondproblem/FibonacciLauncher.java
@@ -0,0 +1,7 @@
+public class FibonacciLauncher {
+  public static void main (String[] args) {
+    System.out.println("Lets find out abour Fibonacci..");
+    Fibonacci first = new Fibonacci();
+    first.getEvens();
+   }
+}
