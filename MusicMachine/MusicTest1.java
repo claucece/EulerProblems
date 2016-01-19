26fc007f2dd78795026c9fc98860a142be952765
Sofia Celi <sceli@thoughtworks.com>
2016-01-19 10:06:19 -0500

    MusicMachine: exception handling

---
+++ MusicMachine/MusicTest1.java
@@ -0,0 +1,14 @@
+import javax.sound.midi.*;
+
+public class MusicTest1 {
+  public void play() {
+    Sequencer sequencer = MidiSystem.getSequencer();
+    // might explode
+    System.out.println("We got a sequencer");
+  }
+  
+public static void main (String[] args) {
+  MusicTest1 mt  = new MusicTest1();
+  mt.play();
+  }
+} 
