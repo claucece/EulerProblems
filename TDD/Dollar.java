25558dba5441f96fa1244023437bce5c448003c1
Sofia Celi <sceli@thoughtworks.com>
2016-01-19 14:08:31 -0500

    refactor

--- TDD/Dollar.java
+++ TDD/Dollar.java
@@ -1,12 +1,16 @@
 class Dollar {
   private int amount;
+  
+  Dollar(int amount) {
+    this.amount = amount;
+  }
 
   Dollar times(int multiplier){
     return new Dollar(amount * multiplier);
   }
   
   public boolean equals(Object object) {
-    Dollar dollar = (dollar) object;
+    Dollar dollar = (Dollar) object;
     return amount == dollar.amount;
   }
 }
