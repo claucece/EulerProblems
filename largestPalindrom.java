15b0eb5abdc81b78050d3f3cf1be57e0327da946
Sofia Celi <sceli@thoughtworks.com>
2016-01-25 10:12:36 -0500

    loop and reverse defined

---
+++ largestPalindrom.java
@@ -0,0 +1,18 @@
+public class largestPalindrom {
+  
+     public void printLargestPalindrom () {
+         for (int i = 100; i < 1000; i++) {
+             for (int j = 100; j < 1000; j ++) {
+                 int multi = i * j;
+                 // System.out.print(multi);
+                 while( multi != 0 )
+                     {
+                     reverse = reverse * 10;
+                     reverse = reverse + n%10;
+                     multi = multi/10;
+                     }
+                 }
+         }
+     }
+
+}
---
+++ largestPalindromLauncher.java
@@ -0,0 +1,7 @@
+public class largestPalindromLauncher {
+     public static void main (String[] args) {
+         System.out.println("Lets see the sum of all multiples of 3 and 5 below 1000");
+         largestPalindrom palindrom = new largestPalindrom();
+         palindrom.printLargestPalindrom();
+}
+}
